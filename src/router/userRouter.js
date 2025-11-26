4ac834a9e93b93c086887fc3c0ce9c6b526cbb00
Yogesh Sadhasivam <yogeshsadhasivam@gmail.com>
2025-11-26 23:11:58 +0530

    Developed Feed API

--- src/router/userRouter.js
+++ src/router/userRouter.js
@@ -3,6 +3,7 @@ const userRouter = express.Router();
 const { userAuth } = require("../middleware/auth.js");
 const Connection = require("../model/connections.js");
 const SAFE_ATTRIBUTES = "firstName lastName age";
+const User = require("../model/user.js");
 
 userRouter.get(
   "/request/received/pending",
@@ -50,4 +51,28 @@ userRouter.get(
   }
 );
 
+userRouter.get("/feed", userAuth, async function (req, res) {
+  try {
+    const loggedInUser = req.user;
+    const existingConnections = await Connection.find({
+      $or: [{ fromUserId: loggedInUser._id }, { toUserId: loggedInUser._id }],
+    });
+    // Build a set of user ids to exclude (users the logged-in user already
+    // has connections with). Ensure we store string ids so $nin matching works
+    // with Mongoose if there are mixed ObjectId/string values.
+    const hideUser = new Set();
+    existingConnections.forEach((connection) => {
+      hideUser.add(String(connection.fromUserId));
+      hideUser.add(String(connection.toUserId));
+    });
+
+    hideUser.add(String(loggedInUser._id));
+
+    const hiddenUser = await User.find({ _id: { $nin: Array.from(hideUser) } }).select(SAFE_ATTRIBUTES);
+
+    return res.status(200).json({ data: hiddenUser });
+  } catch (error) {
+    return res.status(400).json({ message: error.message });
+  }
+});
 module.exports = userRouter;
